16dd25eb9e1bf31d95f38a14c3612441ec33b8e8
Ida Hallbæck <idha0001@stud.kea.dk>
2023-09-07 11:47:20 +0200

    leksikon.js

--- leksikon.js
+++ leksikon.js
@@ -1,9 +1,19 @@
-const urlParams = new URLSearchParams(window.location.search);
-const category = urlParams.get("category");
-const categoryParam = category ? "?category="+category :''
+// https://evadagvbxyuoxojkujjj.supabase.co
+const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV2YWRhZ3ZieHl1b3hvamt1ampqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM5MjM5NjcsImV4cCI6MjAwOTQ5OTk2N30.oui0N1mwqpMeY71DvXuaNMExuUVwIU0gaLPKon7UgiA"
 
-fetch("https://evadagvbxyuoxojkujjj.supabase.co/rest/v1/vildmad_svampe" + categoryParam)
-// header (key)
+fetch("https://evadagvbxyuoxojkujjj.supabase.co/rest/v1/vildmad_svampe",  {
+    method:"GET",
+    headers: {
+        apikey:
+        key
+    },
+})
+
     .then((res) => res.json())
-    .then(showProducts);
+    .then(showData); 
+
+function showData(items) {
+    console.log(items);
+    // items.forEach()
+}
 

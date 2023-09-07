05b904b150f327f21458260d3984d0b3827c14c9
Ida Hallbæck <idha0001@stud.kea.dk>
2023-09-07 13:17:49 +0200

    js eksperimenter

--- leksikon.js
+++ leksikon.js
@@ -1,7 +1,12 @@
+const urlParams = new URLSearchParams(window.location.search);
+const mushroom = urlParams.get("mushroom");
+// const categoryParam = mushroom ? "?mushroom="+mushroom :''
+
+
 // https://evadagvbxyuoxojkujjj.supabase.co
 const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV2YWRhZ3ZieHl1b3hvamt1ampqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM5MjM5NjcsImV4cCI6MjAwOTQ5OTk2N30.oui0N1mwqpMeY71DvXuaNMExuUVwIU0gaLPKon7UgiA"
 
-fetch("https://evadagvbxyuoxojkujjj.supabase.co/rest/v1/vildmad_svampe",  {
+fetch("https://evadagvbxyuoxojkujjj.supabase.co/rest/v1/vildmad_svampe?" ,  {
     method:"GET",
     headers: {
         apikey:
@@ -9,11 +14,24 @@ fetch("https://evadagvbxyuoxojkujjj.supabase.co/rest/v1/vildmad_svampe",  {
     },
 })
 
-    .then((res) => res.json())
-    .then(showData); 
+    .then((res) => res.json()) 
+    .then(showMushroom); 
 
-function showData(items) {
-    console.log(items);
-    // items.forEach()
+function showMushroom(mushrooms){
+    mushrooms.forEach(showMushroom);
+}
+
+function showMushroom(mushroom) {
+    console.log(mushroom);
+    const template = document.querySelector("#lexiconTemplate").content;
+    const copy = template.cloneNode(true);
+
+    copy.querySelector("p.id").textContent = mushroom[0].id;
+    copy.querySelector("p.title").textContent = mushroom[0].title;
+    copy.querySelector("img").src = ``;
+
+copy.querySelector(".billedeKlik").setAttribute("href", `svamp.html?id=${mushroom.id}`);
+document.querySelector("main .lexiconGrid").appendChild(copy);
+
 }
 

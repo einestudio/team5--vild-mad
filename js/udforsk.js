e1a0c602dc090cc79d38eab0148bced64c4bcbab
Ida Hallbæck <idha0001@stud.kea.dk>
2023-09-13 12:19:49 +0200

    yes

--- js/udforsk.js
+++ js/udforsk.js
@@ -1,5 +1,3 @@
-
-
 const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV2YWRhZ3ZieHl1b3hvamt1ampqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM5MjM5NjcsImV4cCI6MjAwOTQ5OTk2N30.oui0N1mwqpMeY71DvXuaNMExuUVwIU0gaLPKon7UgiA"
 
 

a4499a45b7a3d2a557e80ac05c731c3a3b5ea48b
Frederikke <frny0001@stud.kea.dk>
2023-09-13 08:06:23 +0200

    udforsk javascript

---
+++ udforsk.js
@@ -0,0 +1,28 @@
+
+
+const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV2YWRhZ3ZieHl1b3hvamt1ampqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM5MjM5NjcsImV4cCI6MjAwOTQ5OTk2N30.oui0N1mwqpMeY71DvXuaNMExuUVwIU0gaLPKon7UgiA"
+
+
+fetch("https://evadagvbxyuoxojkujjj.supabase.co/rest/v1/vildmad_svampe" ,  {
+    method:"GET",
+    headers: {
+        apikey:
+        key
+    },
+})
+
+
+.then(response => response.json())
+.then(showData) 
+
+function showData(terrain){
+    console.log(terrain);
+    terrain.forEach(element => {
+
+        if (element.tags) {
+            
+            if(element.tags.includes("løvskov"))
+            console.log(element);
+        }
+    });
+}
